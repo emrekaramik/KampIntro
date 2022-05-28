69645f4211567abbba4099baeb6318c425e84c72
Emre Karamik <emresystem7@gmail.com>
2022-05-28 19:08:30 +0300

    28,05,22

---
+++ GenericsIntro/MyList.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GenericsIntro
+{
+    class MyList<T>
+    {
+
+        T[] items;
+        //constructor
+        public MyList()
+        {
+            items= new T[0];  
+        }
+        public void Add(T item)// item yerine her sey yazılabilir 
+        {
+            T[] tempArray = items;
+            items = new T[items.Length+1];
+
+        }
+
+    }
+}
